d5a1d327605ddad296c1c1c9c8cf56095cd0759e
Ethan Griffin <ethangriffin002@gmail.com>
2022-06-05 23:06:38 -0700

    Picks random word on every refresh

--- script.js
+++ script.js
@@ -15298,7 +15298,8 @@ const guessGrid = document.querySelector("[data-guess-grid]");
 const offsetFromDate = new Date(2022, 0, 1)
 const mOffset = Date.now() - offsetFromDate
 const dayOffset = mOffset / 1000 / 60 / 60 / 24
-const targetWord = "havoc";
+const targetWord = targetWords[Math.floor(Math.random() * targetWords.length)];
+console.log(targetWord);
 
 startInteraction()
 
